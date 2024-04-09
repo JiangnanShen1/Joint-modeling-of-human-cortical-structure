5cb339f276c24f424b66fefafeb50445b58dfcc6
Jiangnan Shen <js4377@login2.mccleary.ycrc.yale.edu>
2024-04-09 17:28:02 -0400

    Submit-0409

---
+++ 0920.R
@@ -0,0 +1,108 @@
+####ggnetwork
+library(ggplot2)
+library(geomnet)
+library(ggrepel)
+library(sna)
+library(dplyr)
+
+#数据前处理
+volume<-read.csv(".csv")
+#area
+volume$group <- c(1, 2, 2, 3, 1, 1, 1, 1, 3, 3, 2, 3, 2, 1, 1, 2 ,2, 2, 2, 3 ,1 ,2, 2, 1, 2, 2, 2, 1, 2, 1, 2, 2 ,2 ,1 ,2, 2 ,3, 1, 1 ,1 ,1, 3, 3, 2, 3, 2, 1 ,1, 2 ,2, 2, 2, 3, 1, 2, 2, 1, 2, 2, 2, 1, 2, 1, 2, 2, 2)
+#volume
+#volume$group<-c(2,3,3,4,5,2,2,2,4,4,3,4,3,2,5,3,3,3,3,4,1,3,3,1,3,3,3,1,3,1,3,3,3,2,3,3,4,5,2,2,2,4,4,3,4,3,2,5,3,3,3,3,4,1,3,3,1,3,3,3,1,3,1,3,3,3)
+net<-read.csv("network.csv")
+net<-net%>%select(-X)%>%filter(net==1)%>%filter(Trait1!=Trait2)%>%left_join(volume,by=c("Trait1"="Node"))
+
+#volume$group2 <- c(1,2,3,4,1,1,5,1,2,4,3,4,3,1,1,3,3,3,3,4,5,2,3,5,2,3,3,5,4,5,3,1,2)
+
+
+net <- net[1:(nrow(net)/2),]
+##这里建立了一个连接矩阵，二者有连线就是1，没有就是0
+aaddd<-matrix(0,66,66)
+for (i in 1:length(seq(344,409))) {
+  n1<-net%>%filter(Trait1==seq(344,409)[i])%>%select(Trait2)
+  aaddd[n1$Trait2-343,i]=1
+}
+
+###这里调用了sna里面的函数，重新算了geom_net计算每个点的位置的过程，得到xy
+set.seed(10312016)
+guse<-gplot(aaddd)
+x<-guse[,1]
+y<-guse[,2]
+
+##做归一化
+x1<-(x-min(x))/(max(x)-min(x))
+y1<-(y-min(y))/(max(y)-min(y))
+
+
+####画图前准备
+net$group<-as.character(net$group)
+net$group2<-as.character(net$group2)
+net$Trait1<-as.character(net$Trait1)
+net$Trait2<-as.character(net$Trait2)
+label=unique(net$Trait1)
+
+#画图
+net$color_net <- as.numeric(net$group)
+net$color_net[which(net$color_net==1)] <- "PaleGreen3"
+net$color_net[which(net$color_net==2)] <- "LightBlue2"
+net$color_net[which(net$color_net==3)] <- "PaleVioletRed1"
+
+volume$color_net <- volume$group
+volume$color_net[which(volume$color_net==1)] <- "PaleGreen3"
+volume$color_net[which(volume$color_net==2)] <- "LightBlue2"
+volume$color_net[which(volume$color_net==3)] <- "PaleVioletRed1"
+
+set.seed(10312016)
+ggplot() + 
+  geom_net(data=net,aes(from_id =Trait1, to_id = Trait2,group=group,colour =group,shape=as.character(group2)),layout.alg = 'fruchtermanreingold',
+           linewidth = 0, ealpha = 0.25,  curvature = 0.05,
+           size = 3, vjust = -0.8) +
+  geom_text_repel(aes(x1,y1),color=as.character(volume$color_net),label=label,size=3)+ 
+  theme_net() +
+  theme(legend.position = "bottom") +
+  scale_colour_manual('Trait group',  values=c("PaleGreen3","LightBlue2","PaleVioletRed1")) +
+  scale_shape_manual(values=c(15,16,17,18,19))
+  xlim(c(-0.1,1.1))
+  
+ss <-    ggplot() + 
+    geom_net(data=net,aes(from_id =Trait1, to_id = Trait2,group=group,colour =group),layout.alg = 'fruchtermanreingold',
+             linewidth = 0.3, ealpha = 0.25,  curvature = 0.05,
+             size = 4, vjust = -0.8) +
+    geom_text_repel(aes(x1,y1),color=as.character(volume$color_net),label=label,size=3)+ 
+    theme_net() +
+    theme(legend.position = "bottom") +
+    scale_colour_manual('Trait group',  values=c("PaleGreen3","LightBlue2","PaleVioletRed1")) +
+  xlim(c(-0.1,1.1))
+
+ss2 <-  ggplot() + 
+  geom_net(data=net,aes(from_id =Trait1, to_id = Trait2,group=group,colour =as.character(group2)),layout.alg = 'fruchtermanreingold',
+           linewidth = 0.3, ealpha = 0,  curvature = 0.05,
+           size = 2, vjust = -0.8,ecolour="white") +
+  geom_text_repel(aes(x1,y1),color="red",alpha=0,label=label,size=3)+ 
+  theme_net() +
+  theme(legend.position = "bottom") +
+  scale_colour_manual(values=c("PaleGreen3","LightBlue2","PaleVioletRed1","red","blue")) +
+  xlim(c(-0.1,1.1))
+
+
+ 
+ set.seed(10312016)
+ vp <- viewport()
+ print(ss)
+ set.seed(10312016)
+ print(ss2,vp=vp) 
+ 
+ 
+ #################################
+ ggsave("ss.jpg",ss)
+ sss <- load.image("ss.jpg")
+ 
+ theme_transp_overlay <- theme(
+   panel.background = element_rect(fill="transparent",color=NA),
+   plot.background = element_rect(fill="transparent",color=NA)
+ )
+ 
+ set.seed(10312016)
+ plot(ss)+annotation_custom(ggplotGrob(ss2))
---
+++ analysis_modu.R
@@ -0,0 +1,164 @@
+##################################
+  #install.packages("igraph")
+library(igraph)
+
+measure<-"volume"
+node_info<- read.csv(paste0("/gpfs/gibbs/pi/zhao/js4377/UKB/Cluster/Data/",measure,"_nodes_information.csv"))
+gnet<- read.csv(paste0("/gpfs/gibbs/pi/zhao/js4377/UKB/Cluster/Data/",measure,"_network.csv"))
+pnet<-  read.csv(paste0("/gpfs/gibbs/pi/zhao/js4377/UKB/Cluster/Data/intercept_",measure,"_network.csv"))
+#  randomn<-function(vec num,edge num,clus, rep){
+#    modu<- c()
+# for(i in 1:rep){
+#   random net<-erdos.renyi.game(vec num,edge num, type="gnm",directed = TRUE)
+#   modu<-c(modu,modularity(random net,clus))}
+#   return(modu)
+# }
+# cal_p<-function(observed value, data){zscore<-(observed value-mean(data))/sd(data)return(2*(1-pnorm(abs(z score))))
+
+####Given the genetic correlation, return the top per percent connection
+data_frame_percentage<- function(gg_df, per){ #col gg_df: Trait1, Trait2, weight
+  gg_df<-gg_df[order(-gg_df$weight),]
+  return(gg_df[1:round(per*nrow(gg_df)),])
+} 
+
+# ##Given the network, return the modularity and random network modularity
+# modu<- function(gg_df){ #col gg_df: Trait1, Trait2, weight
+#   gg<- data_frame_percentage(gg_df, per)
+#   ggraph<- graph_from_data_frame(gg,directed =F)
+#   gen_modu<-modularity(ggraph, membership = node_info$cluster_group, weights = gg$weight)
+#   #generate random network
+# }
+
+
+gg_df<-gnet[,c("Trait1", "Trait2", "correlation_rg")]
+colnames(gg_df)[3]<- "weight"
+
+
+res_modu<- data.frame(per=numeric(), gm=numeric(), rm=numeric())
+per_list<- seq(from = 0.001, to = 0.2, by = 0.001)
+for(per in per_list){
+  gg<- data_frame_percentage(gg_df, per)
+  ggraph<- graph_from_data_frame(gg,directed =F)
+  V <- names(as.list(V(ggraph)))
+  membership<- node_info[node_info$Node%in%V,]$cluster_group
+  gen_modu<-modularity(ggraph, membership = membership, weights = gg$weight)
+  ran_modu<-c()
+  for(i in 1:100){
+    ran_modu<-c(ran_modu,modularity(ggraph, membership = sample(membership), weights = gg$weight))
+  }
+  
+  res_modu<- rbind(res_modu, data.frame(per, gm=gen_modu, rm=mean(ran_modu)))
+}
+
+colnames(res_modu)<- c("per", "Genetic Correlations", "Random Networks")
+df_long <- tidyr::pivot_longer(res_modu, cols = c("Genetic Correlations", "Random Networks"), 
+                               names_to = "type", values_to = "modularity")
+
+
+p<- ggplot(data = df_long, aes(x = per, y = modularity, color = type)) +
+  geom_smooth(se = FALSE, size = 1.5) + # 设置线条粗细
+  scale_color_manual(values = c("Genetic Correlations" = "blue", "Random Networks" = "grey"),
+                     #labels = c("Genetic Correlations"= "Genetic Correlations", "Random Networks" = "Random Networks")
+                     ) +
+  labs(
+       x = "Fraction of Strongest Connection",
+       y = "Modularity",
+       color = "Legend") + # 设置图例标题
+  theme_minimal() +
+  theme(panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        axis.line = element_line(colour = "black"),
+        legend.position = "top",
+        legend.title=element_blank()) # 设置图例位置
+
+
+#per<- 0.1
+# p<-ggplot(data = res_modu, aes(x = per)) +
+#   geom_line(aes(y = gm), color = "blue") +
+#   geom_line(aes(y = rm), color = "black") +
+#   labs(title = "Genetic Correlations Random Networks",
+#        x = "Percentage",
+#        y = "Modularity") +
+#   theme_minimal()
+# 
+# print(p)
+# p<-ggplot(data = res_modu, aes(x = per)) +
+#   geom_smooth(aes(y = gm), color = "blue", se = FALSE) +
+#   geom_smooth(aes(y = rm), color = "black", se = FALSE) +
+#   labs(title = "Genetic Correlations Random Networks",
+#        x = "Fraction of Stongest connection",
+#        y = "Modularity") +
+#   theme_minimal() +
+#   theme(panel.grid.major = element_blank(), 
+#         panel.grid.minor = element_blank(),
+#         panel.background = element_blank(),
+#         axis.line = element_line(colour = "black"))
+
+pdf(paste0("/gpfs/gibbs/pi/zhao/js4377/UKB/Cluster/Figure/",measure, "_network.pdf"),width=4, height=4)
+print(p)
+dev.off()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+set.seed(123) # 设置随机种子以便结果可复现
+random_g <- degree.sequence.game(degree_sequence, method = "vl")
+while(!is_connected(random_g)) {
+  random_g <- degree.sequence.game(degree_sequence, method = "vl")
+}
+modularity(random_g, membership = node_info$cluster_group)
+
+
+pg<- subset(pnet, net!=0)[,c("Trait1","Trait2")]
+pgraph<- graph_from_data_frame(pg,directed =F)
+pen_modu<- modularity(pgraph, membership = node_info$cluster_group)
+
+
+##############################
+measure <- "area"
+node_info<- read.csv(paste0("/gpfs/gibbs/pi/zhao/js4377/UKB/Cluster/Data/",measure,"_nodes_information.csv"))
+gnet<- read.csv(paste0("/gpfs/gibbs/pi/zhao/js4377/UKB/Cluster/Data/",measure,"_network.csv"))
+pnet<-  read.csv(paste0("/gpfs/gibbs/pi/zhao/js4377/UKB/Cluster/Data/intercept_",measure,"_network.csv"))
+
+data<- data.frame(genetic_cor=gnet$correlation_rg, phenotypic_cor= pnet$Intercept_rg)
+correlation <- cor(data$genetic_cor, data$phenotypic_cor)
+
+# # Create the scatter plot
+# ggplot(data, aes(x = genetic_cor, y = phenotypic_cor)) +
+#   geom_point(alpha = 0.5) + # Add transparency to points
+#  # geom_smooth(method = "lm", color = "blue") + # Add linear regression line
+#   geom_text(aes(label = paste("r =", round(correlation, 2), ", p < 2.2e-16")), 
+#             x = max(data$genetic_cor), y = max(data$phenotypic_cor), hjust = 1, vjust = 1, color = "red") +
+#   labs(x = "Interregional genetic correlations", 
+#        y = "Interregional phenotypic correlations") +
+#   theme_minimal()
+# 
+# # Print the plot
+# print(p)
+
+
+#Given fraction of the strongest connection, calculate the modularity between the genetic correlation and cluster.
+
